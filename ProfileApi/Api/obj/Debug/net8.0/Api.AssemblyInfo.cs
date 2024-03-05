60cdc72e7a1855c3048386536de2fbc76353648f
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-05 19:49:07 +0500

    Созданы проекты Dal и Logic для QuestionApi

--- ProfileApi/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ ProfileApi/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2b2e52e495fa4035a5c05a50f614b7ea846450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592eb416d400622b480cb9c8fb2f187d4fe050e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
