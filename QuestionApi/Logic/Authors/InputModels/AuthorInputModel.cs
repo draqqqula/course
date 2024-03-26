3233b1583eb1e901c42741915b8e2b4c7929e1b9
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:30:17 +0500

    Добавлены модели для слоя Logic
    InputModel - принимаются в качестве параметров
    ViewModel - возвращаются в методах

---
+++ QuestionApi/Logic/Authors/InputModels/AuthorInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Logic.Authors.Models;
+
+/// <summary>
+/// Модель для добавления автора
+/// </summary>
+public record AuthorInputModel
+{
+    /// <summary>
+    /// Имя автора
+    /// </summary>
+    public required string AuthorName { get; init; }
+}
---
+++ QuestionApi/Logic/Questions/ViewModels/EditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Dal.Edits.Models;
+
+namespace Logic.Questions.ViewModels;
+
+/// <summary>
+/// Модель для получения информации об изменении
+/// </summary>
+public class EditViewModel
+{
+    /// <summary>
+    /// Новый текст
+    /// </summary>
+    public required string Text { get; init; }
+
+    /// <summary>
+    /// Дата редактирования
+    /// </summary>
+    public required DateTime PublishTime { get; init; }
+
+    public static explicit operator EditViewModel(EditDal dal)
+    {
+        return new EditViewModel()
+        {
+            PublishTime = dal.PublishTime,
+            Text = dal.Text
+        };
+    }
+}
