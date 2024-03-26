cfe3144be8587cfd4b9cbdd6c99bb489da2127cb
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:18:40 +0500

    Добавлены интерфейсы для репозиториев

---
+++ QuestionApi/Dal/Answers/Interfaces/IAnswerRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dal.Answers.Models;
+using Dal.Questions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Answers.Interfaces
+{
+    /// <summary>
+    /// Хранение ответов
+    /// </summary>
+    public interface IAnswerRepository
+    {
+        /// <summary>
+        /// Создать ответ
+        /// </summary>
+        /// <param name="answer"></param>
+        /// <returns></returns>
+        public Task<Guid> CreateAsync(AnswerDal answer);
+
+        /// <summary>
+        /// Удалить ответ
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task<Guid?> RemoveAsync(Guid id);
+    }
+}
