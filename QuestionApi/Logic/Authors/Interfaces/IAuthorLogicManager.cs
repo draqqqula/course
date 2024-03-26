d8e7a1c7f726a0d6e22e7a03f2f9382ca16eed59
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:31:08 +0500

    Добавлены интерфейсы для менеджеров

---
+++ QuestionApi/Logic/Authors/Interfaces/IAuthorLogicManager.cs
@@ -0,0 +1,16 @@
+﻿using Logic.Authors.Models;
+
+namespace Logic.Authors.Interfaces;
+
+/// <summary>
+/// Работа с авторами
+/// </summary>
+public interface IAuthorLogicManager
+{
+    /// <summary>
+    /// Создать автора
+    /// </summary>
+    /// <param name="author"></param>
+    /// <returns></returns>
+    public Task<Guid> CreateAsync(AuthorInputModel author);
+}
---
+++ QuestionApi/Logic/Questions/Interfaces/IQuestionLogicManager.cs
@@ -0,0 +1,45 @@
+﻿using Logic.Questions.Models;
+using Logic.Questions.ViewModels;
+
+namespace Logic.Questions.Interfaces;
+
+/// <summary>
+/// Управление вопросами
+/// </summary>
+public interface IQuestionLogicManager
+{
+    /// <summary>
+    /// Опубликовать вопрос
+    /// </summary>
+    /// <param name="question"></param>
+    /// <returns></returns>
+    public Task<Guid> PublishAsync(QuestionInputModel question);
+
+    /// <summary>
+    /// Поиск по вопросам
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public Task<SearchResultViewModel> SearchAsync(SearchOptionsInputModel options);
+
+    /// <summary>
+    /// Получить детали вопроса
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <returns></returns>
+    public Task<QuestionDetailViewModel> GetInfoAsync(Guid Id);
+
+    /// <summary>
+    /// Удалить вопрос
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public Task<Guid?> RemoveAsync(Guid id);
+
+    /// <summary>
+    /// Отредактировать вопрос
+    /// </summary>
+    /// <param name="edit"></param>
+    /// <returns></returns>
+    public Task<Guid> EditAsync(EditInputModel edit);
+}
