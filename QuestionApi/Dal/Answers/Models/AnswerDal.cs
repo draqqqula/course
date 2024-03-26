2f5dd87660602336f944d97c0d25378f20ee3fba
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:17:43 +0500

    Добавлены Dal-сущности
    QuestionDal - вопрос
    AnswerDal - ответ
    AuthorDal - автор
    EditDal - редактирование
    TagDal - тег

---
+++ QuestionApi/Dal/Answers/Models/AnswerDal.cs
@@ -0,0 +1,26 @@
+﻿using Core.Dal.Base;
+using Dal.Authors.Models;
+using Dal.Questions.Models;
+
+namespace Dal.Answers.Models;
+
+/// <summary>
+/// Отражение модели ответа из хранилища данных
+/// </summary>
+public record class AnswerDal : BaseEntityDal<Guid>
+{
+    /// <summary>
+    /// Текст ответа
+    /// </summary>
+    public required string Text { get; init; }
+
+    /// <summary>
+    /// Автор ответа
+    /// </summary>
+    public virtual required AuthorDal Author { get; init; }
+
+    /// <summary>
+    /// Вопрос, на который сделан ответ
+    /// </summary>
+    public virtual required QuestionDal Question { get; init; }
+}
---
+++ QuestionApi/Dal/Edits/Models/EditDal.cs
@@ -0,0 +1,25 @@
+﻿using Core.Dal.Base;
+using Dal.Questions.Models;
+
+namespace Dal.Edits.Models;
+
+/// <summary>
+/// Отражение модели изменения вопросов из хранилища данных
+/// </summary>
+public record EditDal : BaseEntityDal<Guid>
+{
+    /// <summary>
+    /// Новый текст вопроса
+    /// </summary>
+    public required string Text { get; init; }
+
+    /// <summary>
+    /// Время добавления изменения
+    /// </summary>
+    public required DateTime PublishTime { get; init; }
+
+    /// <summary>
+    /// Отредактированный вопрос
+    /// </summary>
+    public virtual required QuestionDal Question { get; init; }
+}
