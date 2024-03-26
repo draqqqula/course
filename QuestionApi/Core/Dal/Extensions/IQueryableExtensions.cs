cc77b74f4af02e21ff421a23ecd3eceb918a0746
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:39:06 +0500

    Добавлены методы-расширения для IQueryable

---
+++ QuestionApi/Core/Dal/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,74 @@
+﻿namespace Core.Dal.Extensions;
+using System.Linq;
+using System.Linq.Expressions;
+using System;
+
+/// <summary>
+/// Расширения для IQueryable
+/// </summary>
+public static class IQueryableExtensions
+{
+    /// <summary>
+    /// Фильтрует коллекцию только если <paramref name="condition"/> вернёт true
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="collection"></param>
+    /// <param name="condition"></param>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public static IQueryable<T> WhereIf<T>(
+        this IQueryable<T> collection, 
+        bool condition,
+        Expression<Func<T, bool>> predicate)
+    {
+        if (condition)
+        {
+            return collection.Where(predicate);
+        }
+        return collection;
+    }
+
+    /// <summary>
+    /// Фильтрует коллекцию только если <paramref name="nullable"/> будет не null
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="collection"></param>
+    /// <param name="nullable"></param>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public static IQueryable<T> WhereIfNotNull<T>(
+        this IQueryable<T> collection,
+        object? nullable,
+        Expression<Func<T, bool>> predicate)
+    {
+        return collection.WhereIf(nullable is not null, predicate);
+    }
+
+    public static PageInfo<T> ToPageInfo<T>(
+        this IQueryable<T> collection,
+        int pageNumber,
+        int perPage)
+    {
+        var total = collection.Count();
+        if (pageNumber <= 0 || perPage <= 0)
+        {
+            return new PageInfo<T>()
+            {
+                CurrentPage = 0,
+                TotalPages = 0,
+                Collection = collection,
+            };
+        }
+        var totalPages = (int)Math.Ceiling((double)total / perPage);
+        var currentPage = Math.Clamp(pageNumber, 1, totalPages);
+        var pages = collection
+            .Skip((currentPage - 1) * perPage)
+            .Take(perPage);
+        return new PageInfo<T>()
+        {
+            CurrentPage = currentPage,
+            TotalPages = totalPages,
+            Collection = pages,
+        };
+    }
+}
---
+++ QuestionApi/Core/Dal/Extensions/PageInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Dal.Extensions;
+
+public record PageInfo<T>
+{
+    public required int CurrentPage { get; init; }
+    public required int TotalPages { get; init; }
+    public required IQueryable<T> Collection { get; init; }
+}
