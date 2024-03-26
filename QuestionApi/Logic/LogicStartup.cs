fea902afcd438d02f263171b79072311e7dd7434
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:32:43 +0500

    Добавлен стартап для слоя Logic

---
+++ QuestionApi/Logic/LogicStartup.cs
@@ -0,0 +1,21 @@
+﻿using Logic.Answers;
+using Logic.Answers.Interfaces;
+using Logic.Authors;
+using Logic.Authors.Interfaces;
+using Logic.Questions;
+using Logic.Questions.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Logic;
+
+public static class LogicStartUp
+{
+    public static IServiceCollection TryAddLogic(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.TryAddScoped<IAnswerLogicManager, AnswerLogicManager>();
+        serviceCollection.TryAddScoped<IAuthorLogicManager, AuthorLogicManager>();
+        serviceCollection.TryAddScoped<IQuestionLogicManager, QuestionLogicManager>();
+        return serviceCollection;
+    }
+}
