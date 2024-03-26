ea4470f6b76434514b1964ae0fc8b330ad40861f
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:18:40 +0500

    Добавлены интерфейсы для репозиториев

---
+++ QuestionApi/Dal/Edits/Interfaces/IEditRepository.cs
@@ -0,0 +1,16 @@
+﻿using Dal.Edits.Models;
+
+namespace Dal.Edits.Interfaces;
+
+/// <summary>
+/// Хранилище изменений вопросов
+/// </summary>
+public interface IEditRepository
+{
+    /// <summary>
+    /// Добавить изменение
+    /// </summary>
+    /// <param name="dal"></param>
+    /// <returns></returns>
+    public Task<Guid> AddAsync(EditDal dal);
+}
---
+++ QuestionApi/Dal/Questions/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,44 @@
+﻿using Dal.Questions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal.Questions.Interfaces;
+
+/// <summary>
+/// Хранение вопросов
+/// </summary>
+public interface IQuestionRepository
+{
+    /// <summary>
+    /// Создать вопрос
+    /// </summary>
+    /// <param name="dal"></param>
+    /// <returns></returns>
+    public Task<Guid> CreateAsync(QuestionDal dal);
+
+    /// <summary>
+    /// Получить все вопросы
+    /// </summary>
+    /// <returns></returns>
+    public IQueryable<QuestionDal> GetAll();
+
+    /// <summary>
+    /// Получить информацию о вопросе
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public Task<QuestionDal?> GetInfoAsync(Guid id);
+
+    /// <summary>
+    /// Отметить ответ как решение
+    /// </summary>
+    /// <param name="answerId"></param>
+    /// <returns></returns>
+    public Task<Guid?> AcceptSolutionAsync(Guid answerId);
+
+    /// <summary>
+    /// Удалить вопрос
+    /// </summary>
+    /// <param name="answerId"></param>
+    /// <returns></returns>
+    public Task<Guid?> RemoveAsync(Guid answerId);
+}
