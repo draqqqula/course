9c19b0eea65c63158e0468b62efd7e34ed27e981
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:38:01 +0500

    Добавлен фильтр на свои исключения

---
+++ QuestionApi/Api/Filters/RemovalExceptionFilter.cs
@@ -0,0 +1,39 @@
+﻿using Core.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters;
+
+/// <summary>
+/// Фильтр для методов удаления данных
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class RemovalExceptionFilter : Attribute, IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is DataNotFoundException)
+        {
+            Handle(context, 404);
+        }
+        else if (context.Exception is ForbiddenRemovalException)
+        {
+            Handle(context, 400);
+        }
+    }
+
+    /// <summary>
+    /// Изменяет статус-код ответа на <paramref name="statusCode"/> и содержимое ответа на текст ошибки.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="statusCode"></param>
+    private static void Handle(ExceptionContext context, int statusCode)
+    {
+        context.HttpContext.Response.StatusCode = statusCode;
+        context.ExceptionHandled = true;
+        context.Result = new ContentResult
+        {
+            Content = context.Exception.Message
+        };
+    }
+}
