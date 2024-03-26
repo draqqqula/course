6db55f6edfa7dde6c657e63cfe5ba1e1505202f3
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-03-27 00:39:37 +0500

    Добавлен проект с тестами

---
+++ QuestionApi/DalTest/AuthorTests.cs
@@ -0,0 +1,39 @@
+﻿using Dal;
+using Dal.Authors.Interfaces;
+using Dal.Authors.Models;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalTest
+{
+    [TestFixture]
+    public class AuthorTests
+    {
+        [Test]
+        public async Task CheckCreateAuthor()
+        {
+            var services = new ServiceCollection();
+            DalStartup.TryAddDal(services);
+            var provider = new DefaultServiceProviderFactory().CreateServiceProvider(services);
+            provider.CreateScope();
+            var context = provider.GetService<QuestionDbContext>();
+            var repository = provider.GetService<IAuthorRepository>();
+
+            var name = "Jonny";
+
+            var author = new AuthorDal
+            {
+                Name = name 
+            };
+
+            var id = await repository.CreateAsync(author);
+            var entry = await repository.GetInfoAsync(id);
+            Assert.That(entry.Name == name);
+        }
+    }
+}
---
+++ QuestionApi/DalTest/TagTests.cs
@@ -0,0 +1,39 @@
+﻿using Dal;
+using Dal.Authors.Interfaces;
+using Dal.Authors.Models;
+using Dal.Tags.Interfaces;
+using Dal.Tags.Models;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalTest
+{
+    [TestFixture]
+    public class TagTests
+    {
+        [Test]
+        public async Task CheckCreateTag()
+        {
+            var services = new ServiceCollection();
+            DalStartup.TryAddDal(services);
+            var provider = new DefaultServiceProviderFactory().CreateServiceProvider(services);
+            provider.CreateScope();
+            var context = provider.GetService<QuestionDbContext>();
+            var repository = provider.GetService<ITagRepository>();
+
+            var text = "Foo";
+
+            await repository.AddRangeAsync([new TagDal() { Text = text }]);
+
+            var found = await repository.FindTagAsync(text);
+
+            Assert.That(found is not null);
+            Assert.That(found.Text == text);
+        }
+    }
+}
