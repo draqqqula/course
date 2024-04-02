693373f1d67b724ced8f6b13f2ea3366a46d6b7f
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-04-02 21:09:29 +0500

    проект Core перенесён в отдельную от QuestionApi папку

---
+++ Libs/Core/Exceptions/DataNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Exceptions;
+
+/// <summary>
+/// Исключение если запись по запросу не найдена
+/// </summary>
+public class DataNotFoundException : Exception
+{
+    public DataNotFoundException(string? message) : base(message)
+    {
+    }
+}
---
+++ Libs/Core/Exceptions/ForbiddenRemovalException.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Exceptions;
+
+/// <summary>
+/// Исключение если нельзя удалить запись
+/// </summary>
+public class ForbiddenRemovalException : Exception
+{
+    public ForbiddenRemovalException(string? message) : base(message)
+    {
+
+    }
+}
