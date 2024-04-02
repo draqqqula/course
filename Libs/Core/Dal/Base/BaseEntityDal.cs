3805e2b69255a3ebad1cac6cdc0035aebcacd77c
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-04-02 21:09:29 +0500

    проект Core перенесён в отдельную от QuestionApi папку

---
+++ Libs/Core/Dal/Base/BaseEntityDal.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Dal.Base;
+
+/// <summary>
+/// Базовая сущность для работы с сущностями в бд
+/// </summary>
+/// <typeparam name="T">тип идентификатор</typeparam>
+public abstract record BaseEntityDal<T>
+{
+    /// <summary>
+    /// уникальный идентфиикатор сущности
+    /// </summary>
+    public T Id { get; init; }
+}
