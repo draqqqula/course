1bfe7ad7d3a1c40803a27460ea7eb20c8cc262e9
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-16 00:41:34 +0500

    Dto CheckUserExists

---
+++ Libs/MyProfileConnectionLib/MyProfileConnectionLib/ConnectionServices/DtoModels/CheckUserExists/CheckUserExistsProfileApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProfileConnectionLib.ConnectionServices.DtoModels.CheckUserExists;
+
+public record CheckUserExistsProfileApiRequest
+{
+    public required Guid Id { get; init; }
+}
