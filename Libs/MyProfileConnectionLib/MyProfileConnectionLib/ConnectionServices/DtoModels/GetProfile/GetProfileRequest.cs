fb4dbfa482d2d9220259748963ab1290c79104d6
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-16 00:42:01 +0500

    Dto GetProfile

---
+++ Libs/MyProfileConnectionLib/MyProfileConnectionLib/ConnectionServices/DtoModels/GetProfile/GetProfileRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProfileConnectionLib.ConnectionServices.DtoModels.GetUserInfo;
+
+public record GetProfileRequest
+{
+    public required Guid Id { get; init; }
+}
