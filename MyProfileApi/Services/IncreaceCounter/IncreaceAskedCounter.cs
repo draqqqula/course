db3c678d89eb478df94e1adf0805289c150d099a
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-15 20:07:18 +0500

    Добавлен Application ProfileApi

---
+++ MyProfileApi/Services/IncreaceCounter/IncreaceAskedCounter.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.IncreaceCounter;
+
+internal class IncreaceAskedCounter : IncreaceCounterBase
+{
+    public IncreaceAskedCounter(ITakeProfile takeProfile, IModifyProfile modifyProfile) : base(takeProfile, modifyProfile)
+    {
+    }
+
+    protected override void Increace(Profile profile, int amount)
+    {
+        profile.AskedCount += (uint)amount;
+    }
+}
---
+++ MyProfileApi/Services/Interfaces/ICreateProfile.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Services.Interfaces;
+
+public interface ICreateProfile
+{
+    public Task<Guid> CreateProfileAsync(Profile profile);
+}
