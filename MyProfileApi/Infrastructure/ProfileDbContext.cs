6feeac8ec9a34cfa3ff52fdede1353cc78492206
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-15 20:06:49 +0500

    Добавлена Infrastructure ProfileApi

---
+++ MyProfileApi/Infrastructure/ProfileDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure;
+
+internal class ProfileDbContext : DbContext
+{
+    private const string ConnectionName = "DefaultConnection";
+
+    public DbSet<Profile> Profiles { get; set; }
+
+    public ProfileDbContext() : base()
+    {
+        AppSettings = new ConfigurationBuilder()
+        .Add(new JsonConfigurationSource { Path = "datasettings.json", ReloadOnChange = true })
+        .Build();
+    }
+
+    public IConfigurationRoot AppSettings { get; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = AppSettings.GetConnectionString(ConnectionName);
+        optionsBuilder.UseNpgsql(connectionString);
+        optionsBuilder.UseLazyLoadingProxies();
+    }
+}
---
+++ MyProfileApi/Infrastructure/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure;
+
+public static class StartUp
+{
+    public static IServiceCollection TryAddInfrastructure(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.TryAddScoped<ProfileDbContext>();
+        serviceCollection.TryAddScoped<ProfileRepository>();
+        serviceCollection.TryAddScoped<IStoreProfile>(provider => provider.GetRequiredService<ProfileRepository>());
+        serviceCollection.TryAddScoped<ITakeProfile>(provider => provider.GetRequiredService<ProfileRepository>());
+        serviceCollection.TryAddScoped<IModifyProfile>(provider => provider.GetRequiredService<ProfileRepository>());
+        return serviceCollection;
+    }
+}
