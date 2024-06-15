1282d6f2e3edd279d850a0768223117fe50bbd0c
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-16 00:42:55 +0500

    Dto IncreaceCounterBase

---
+++ Libs/MyProfileConnectionLib/MyProfileConnectionLib/ConnectionServices/DtoModels/IncreaceCounterRequestBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProfileConnectionLib.ConnectionServices.DtoModels;
+
+public abstract record IncreaceCounterRequestBase
+{
+    public required Guid Id { get; init; }
+    public required int Amount { get; init; }
+}
