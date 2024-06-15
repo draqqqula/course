6c3fcaed650db18ce28d9cd49070685d5004307a
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-16 00:42:34 +0500

    Dto IncreaceAaskedCounter

---
+++ Libs/MyProfileConnectionLib/MyProfileConnectionLib/ConnectionServices/DtoModels/IncreaceAskedCounter/IncreaceAskedCounterResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProfileConnectionLib.ConnectionServices.DtoModels.IncreaceAskedCounter;
+
+public record IncreaceAskedCounterResponse : IncreaceCounterResponseBase
+{
+}
