4a8039d15e44b5fda7fa0f6c1b170a3fdfcf0ccb
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-18 19:54:36 +0500

    Добавлена фабрика каналов RabbitMQ в Core

---
+++ Libs/Core/Logic/Connections/RabbitMQ/RabbitMQChannelFactory.cs
@@ -0,0 +1,34 @@
+﻿using Core.Logic.Connections.RabbitMQ.Interfaces;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Logic.Connections.RabbitMQ;
+
+internal class RabbitMQChannelFactory : IRabbitMQChannelFactory
+{
+    private readonly IConnection _connection;
+
+    public RabbitMQChannelFactory(IConfiguration configuration)
+    {
+        IConnectionFactory connectionFactory;
+        if (configuration["RabbitMQConnectionType"] == "Uri")
+        {
+            connectionFactory = new ConnectionFactory() { Uri = configuration.GetValue<Uri>("AmqpUri") };
+        }
+        else
+        {
+            connectionFactory = new ConnectionFactory() { HostName = configuration.GetValue<string>("RabbitMQHostName") };
+        }
+        _connection = connectionFactory.CreateConnection();
+    }
+
+    public IModel Create()
+    {
+        return _connection.CreateModel();
+    }
+}
