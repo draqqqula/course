514aacd7a7134b9e4a78214be9a54ae586b5816b
draqqqula <121135567+draqqqula@users.noreply.github.com>
2024-06-18 19:55:06 +0500

    добавлен паблишер для RabbitMQ в Core

---
+++ Libs/Core/Logic/Connections/RabbitMQ/RabbitMQPublisher.cs
@@ -0,0 +1,21 @@
+﻿using Core.Logic.Connections.RabbitMQ.Interfaces;
+using Core.Logic.Connections.RabbitMQ.Models;
+using Core.Logic.Serialization.Interfaces;
+using RabbitMQ.Client;
+
+namespace Core.Logic.Connections.RabbitMQ;
+
+internal class RabbitMQPublisher : IRabbitMQPublisher
+{
+    private readonly IBinarySerializer _serializer;
+    public RabbitMQPublisher(IBinarySerializer serializer)
+    {
+        _serializer = serializer;
+    }
+
+    public void Publish<T>(T message, IModel channel, PublishArguments arguments)
+    {
+        var data = _serializer.Serialize(message);
+        channel.BasicPublish(arguments.ExchangeName, arguments.RoutingKey, arguments.Properties, data);
+    }
+}
